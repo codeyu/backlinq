c3f9f283d84781053b1d7cbb10ab0ebe2a597cbb
azizatif <azizatif@e924b16c-dd53-0410-a5ef-1310a270471b>
2008-08-07 20:55:19 +0000

    Initial import.

---
+++ trunk/src/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BackLINQ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BackLINQ")]
+[assembly: AssemblyCopyright("Copyright (c) 2008, Atif Aziz. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d8ca19d-f7ef-47f2-8144-4099aab2b2b7")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
