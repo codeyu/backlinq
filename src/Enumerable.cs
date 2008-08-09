bd3f948908d9ff734075f843521b08775a6d617f
azizatif <azizatif@e924b16c-dd53-0410-a5ef-1310a270471b>
2008-08-09 21:23:24 +0000

    Added implementation of DefaultIfEmpty standard query operator.

--- src/Enumerable.cs
+++ src/Enumerable.cs
@@ -956,6 +956,38 @@ namespace BackLinq
             return first.Concat(second).Distinct(comparer);
         }
 
+        /// <summary>
+        /// Returns the elements of the specified sequence or the type 
+        /// parameter's default value in a singleton collection if the 
+        /// sequence is empty.
+        /// </summary>
+
+        public static IEnumerable<TSource> DefaultIfEmpty<TSource>(
+            this IEnumerable<TSource> source)
+        {
+            return source.DefaultIfEmpty(default(TSource));
+        }
+
+        /// <summary>
+        /// Returns the elements of the specified sequence or the specified 
+        /// value in a singleton collection if the sequence is empty.
+        /// </summary>
+        
+        public static IEnumerable<TSource> DefaultIfEmpty<TSource>(
+            this IEnumerable<TSource> source,
+            TSource defaultValue)
+        {
+            CheckNotNull(source, "source");
+
+            using (var e = source.GetEnumerator())
+            {
+                if (!e.MoveNext())
+                    yield return defaultValue;
+
+                do { yield return e.Current; } while (e.MoveNext());
+            }
+        }
+
         private static void CheckNotNull<T>(T value, string name) where T : class
         {
             if (value == null) 
