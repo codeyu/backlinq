87edc084f9ba3374c63326860f97ecc0bf77ccae
azizatif <azizatif@e924b16c-dd53-0410-a5ef-1310a270471b>
2008-08-09 21:56:58 +0000

    Added implementation of SequenceEqual standard query operator.

--- src/Enumerable.cs
+++ src/Enumerable.cs
@@ -1060,6 +1060,51 @@ namespace BackLinq
                  : source.Any(item => comparer.Equals(item, value));
         }
 
+        /// <summary>
+        /// Determines whether two sequences are equal by comparing the 
+        /// elements by using the default equality comparer for their type.
+        /// </summary>
+
+        public static bool SequenceEqual<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            return first.SequenceEqual(second, /* comparer */ null);
+        }
+
+        /// <summary>
+        /// Determines whether two sequences are equal by comparing their 
+        /// elements by using a specified <see cref="IEqualityComparer{T}" />.
+        /// </summary>
+
+        public static bool SequenceEqual<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            IEqualityComparer<TSource> comparer)
+        {
+            CheckNotNull(first, "frist");
+            CheckNotNull(second, "second");
+
+            comparer = comparer ?? EqualityComparer<TSource>.Default;
+
+            using (IEnumerator<TSource> lhs = first.GetEnumerator(), 
+                                        rhs = second.GetEnumerator())
+            {
+                do
+                {
+                    if (!lhs.MoveNext())
+                        return !rhs.MoveNext();
+
+                    if (!rhs.MoveNext())
+                        return false;
+                } 
+                while (comparer.Equals(lhs.Current, rhs.Current));
+            }
+
+            return false;
+        }
+
+        [DebuggerStepThrough]
         private static void CheckNotNull<T>(T value, string name) where T : class
         {
             if (value == null) 
