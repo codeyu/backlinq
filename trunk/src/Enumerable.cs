b68f693a9cba84217bd9bcfc3cbd737b1cb02ddb
azizatif <azizatif@e924b16c-dd53-0410-a5ef-1310a270471b>
2008-08-10 11:01:13 +0000

    Added implementation of ToDictionary standard query operator.

--- trunk/src/Enumerable.cs
+++ trunk/src/Enumerable.cs
@@ -1345,6 +1345,85 @@ namespace BackLinq
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Creates a <see cref="Dictionary{TKey,TValue}" /> from an 
+        /// <see cref="IEnumerable{T}" /> according to a specified key 
+        /// selector function.
+        /// </summary>
+
+        public static Dictionary<TKey, TSource> ToDictionary<TSource, TKey>(
+            this IEnumerable<TSource> source, 
+            Func<TSource, TKey> keySelector)
+        {
+            return source.ToDictionary(keySelector, /* comparer */ null);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Dictionary{TKey,TValue}" /> from an 
+        /// <see cref="IEnumerable{T}" /> according to a specified key 
+        /// selector function and key comparer.
+        /// </summary>
+
+        public static Dictionary<TKey, TSource> ToDictionary<TSource, TKey>(
+            this IEnumerable<TSource> source, 
+            Func<TSource, TKey> keySelector, 
+            IEqualityComparer<TKey> comparer)
+        {
+            return source.ToDictionary(keySelector, e => e);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Dictionary{TKey,TValue}" /> from an 
+        /// <see cref="IEnumerable{T}" /> according to specified key 
+        /// selector and element selector functions.
+        /// </summary>
+        
+        public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(
+            this IEnumerable<TSource> source, 
+            Func<TSource, TKey> keySelector, 
+            Func<TSource, TElement> elementSelector)
+        {
+            return source.ToDictionary(keySelector, elementSelector, /* comparer */ null);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Dictionary{TKey,TValue}" /> from an 
+        /// <see cref="IEnumerable{T}" /> according to a specified key 
+        /// selector function, a comparer, and an element selector function.
+        /// </summary>
+
+        public static Dictionary<TKey, TElement> ToDictionary<TSource, TKey, TElement>(
+            this IEnumerable<TSource> source, 
+            Func<TSource, TKey> keySelector, 
+            Func<TSource, TElement> elementSelector, 
+            IEqualityComparer<TKey> comparer)
+        {
+            CheckNotNull(source, "source");
+            CheckNotNull(keySelector, "keySelector");
+            CheckNotNull(elementSelector, "elementSelector");
+
+            var dict = new Dictionary<TKey, TElement>(comparer);
+
+            foreach (var item in source)
+            {
+                //
+                // ToDictionary is meant to throw ArgumentNullException if
+                // keySelector produces a key that is null and 
+                // Argument exception if keySelector produces duplicate keys 
+                // for two elements. Incidentally, the doucmentation for
+                // IDictionary<TKey, TValue>.Add says that the Add method
+                // throws the same exceptions under the same circumstances
+                // so we don't need to do any additional checking or work
+                // here and let the Add implementation do all the heavy
+                // lifting.
+                //
+
+                dict.Add(keySelector(item), elementSelector(item));
+            }
+
+            return dict;
+        }
+
         [DebuggerStepThrough]
         private static void CheckNotNull<T>(T value, string name) where T : class
         {
