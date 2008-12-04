af351c71efc9994bd5c8d88654c843e636e41a3b
azizatif <azizatif@e924b16c-dd53-0410-a5ef-1310a270471b>
2008-12-04 21:06:34 +0000

    Added quotes around solution path in build batch script to avoid issues if the distribution root is under a path with a long file name component.

--- trunk/build.cmd
+++ trunk/build.cmd
@@ -27,7 +27,5 @@ REM -------------------------------------------------------------------------
 REM
 
 for %%i in (Debug Release) do (
-    "%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild" /p:Configuration=%%i %~dp0src\BackLINQ.sln
+    "%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild" /p:Configuration=%%i "%~dp0src\BackLINQ.sln"
 )
-
-
